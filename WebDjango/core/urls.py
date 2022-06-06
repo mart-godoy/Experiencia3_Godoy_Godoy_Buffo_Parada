1c7ab19f695110a7e982b0b9dfbc53bdcbb49797
MatiaSinS <matia.godoyg@duocuc.cl>
2022-06-06 19:57:50 -0400

    Implementando paginas con formularios y base de datos

---
+++ WebDjango/core/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import index,formsuge,compra,nosotros,venta,creacionuser,leer,ventleer,registrarUsuario
+from . import views
+urlpatterns = [
+    path('',index,name="index"),
+    path('index',index,name="index"),
+    path('formsuge',formsuge,name="formsuge"),
+    path('compra',compra,name="compra"),
+    path('nosotros',nosotros,name="nosotros"),
+    path('venta',venta,name="venta"),
+    path('creacionuser',creacionuser,name="creacionuser"),
+    path('leer',leer,name="leer"),
+    path('ventleer',ventleer,name="ventleer"),
+    path('registrarUsuario/', views.registrarUsuario),
+]
+
+# path('',,name=""),
