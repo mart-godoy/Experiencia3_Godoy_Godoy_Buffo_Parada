cfb2e1ab139dd68da4d87c3fb14543e0babcee13
MARTIN . GODOY OYARZUN <mart.godoy@duocuc.cl>
2022-06-10 04:21:58 -0400

    Finalizando Experiencia 3 con mejoras al sitio.

--- WebDjango/core/urls.py
+++ WebDjango/core/urls.py
@@ -1,18 +1,22 @@
 from django.urls import path
-from .views import index,formsuge,compra,nosotros,venta,creacionuser,leer,ventleer,registrarUsuario,modificar
+from .views import index,formsuge,compra,nosotros,venta,creacionuser,leer,ventleer,registrarUsuario,modUsuario,modifUsuario,registrarEnvio,modifEnvioo,modEnvioo
 from . import views
 urlpatterns = [
     path('',index,name="index"),
     path('index',index,name="index"),
     path('formsuge',formsuge,name="formsuge"),
-    path('compra',compra,name="compra"),
     path('nosotros',nosotros,name="nosotros"),
+    path('compra',compra,name="compra"),
     path('venta',venta,name="venta"),
+    path('registrarEnvio/', registrarEnvio,name="registrarEnvio"),
+    path('modEnvioo/', views.modEnvioo, name="modEnvioo"),
+    path('modifEnvioo/', views.modifEnvioo, name="modifEnvioo"),
+    path('ventleer',ventleer,name="ventleer"),
     path('creacionuser',creacionuser,name="creacionuser"),
     path('leer',leer,name="leer"),
-    path('ventleer',ventleer,name="ventleer"),
     path('registrarUsuario/', views.registrarUsuario),
-    path('modificar',modificar,name="modificar"),
+    path('modUsuario/', modUsuario, name="modUsuario"),
+    path('modifUsuario/', modifUsuario, name="modifUsuario"),
 ]
 
 # path('',,name=""),
--- WebDjango/core/views.py
+++ WebDjango/core/views.py
@@ -4,46 +4,64 @@ from .models import Usuario, Envio
 # Create your views here.
 
 def index(request):
-    
+
     return render(request,'core/index.html')
 
 def formsuge(request):
-    
+
     return render(request,'core/formsuge.html')
 
 def compra(request):
-    
+
     return render(request,'core/compra.html')
 
 
 def nosotros(request):
-    
+
     return render(request,'core/nosotros.html')
 
 
 def venta(request):
 
+
     return render(request,'core/venta.html')
 
 def creacionuser(request):
-
-
-
+    
     return render(request,'core/creacionuser.html')
 
 def leer(request):
-    
+
     usuariosLista = Usuario.objects.all()
     return render(request,"core/leer.html",{"usuarios": usuariosLista})
+    
+def modUsuario(request):
+
+    usuarioMod = Usuario.objects.all()
+    return render(request,"core/modUsuario.html",{"modificara":usuarioMod})
+
+def modifUsuario(request):
+    usuario = request.POST['usuario']
+    nombre = request.POST['nombre']
+    password = request.POST['password']
+    correo = request.POST['correo']
+    telefono = request.POST['telefono']
+
+    usuarioMod = Usuario.objects.all()
+    usuarioMod.usuario = usuario
+    usuarioMod.nombre = nombre
+    usuarioMod.password = password
+    usuarioMod.correo = correo
+    usuarioMod.telefono = telefono
+    usuarioMod.save()
+
+    return redirect('/')
 
 def ventleer(request):
 
-    envios = Envio.objects.all()
+    enviosLista = Envio.objects.all()
+    return render(request,"core/ventleer.html",{"envios": enviosLista})
 
-    datos= {
-        'envios': envios
-    }
-    return render(request, 'core/ventleer.html',datos)
 
 def registrarUsuario(request):
     usuario = request.POST['usuario']
@@ -56,6 +74,48 @@ def registrarUsuario(request):
 
     return redirect('/')
 
-def modificar(request):
 
-    return render(request, 'core/modificar.hmtl')
+def registrarEnvio(request):
+    rut = request.POST['rut']
+    nombre = request.POST['nombre']
+    email = request.POST['email']
+    phone = request.POST['phone']
+    ciudad = request.POST['ciudad']
+    comuna = request.POST['comuna']
+    calle = request.POST['calle']
+    casa = request.POST['casa']
+
+    envios = Envio.objects.create(
+        rut=rut, nombre=nombre, email=email, phone=phone, ciudad=ciudad,
+        comuna=comuna,calle=calle,casa=casa
+    )
+
+    return redirect('/')
+
+def modEnvioo(request):
+
+    envioMod = Usuario.objects.all()
+    return render(request,"core/modEnvio.html",{"modificar":envioMod})
+
+def modifEnvioo(request):
+    rut = request.POST['rut']
+    nombre = request.POST['nombre']
+    email = request.POST['email']
+    phone = request.POST['phone']
+    ciudad = request.POST['ciudad']
+    comuna = request.POST['comuna']
+    calle = request.POST['calle']
+    casa = request.POST['casa']
+
+    envioMod = Envio.objects.all()
+    envioMod.rut = rut
+    envioMod.nombre = nombre
+    envioMod.email = email
+    envioMod.phone = phone
+    envioMod.ciudad = ciudad
+    envioMod.comuna = comuna
+    envioMod.calle = calle
+    envioMod.casa = casa
+    envioMod.save()
+
+    return redirect('/')
